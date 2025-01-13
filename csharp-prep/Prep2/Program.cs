5e58a3cdd686719be5c601f8a59bb9b59bb8b514
coolmen444 <isaac.edwards.20022@gmail.com>
2025-01-12 20:57:32 -0700

    Added code for grade checking program and James Bond program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        float percentage = float.Parse(Console.ReadLine());
+        string letter = "";
+        bool passed = true;
+
+        if (percentage >= 90) {
+            letter = "A";
+        }
+        else if (percentage >= 80) {
+            letter = "B";
+        }
+        else if (percentage >= 70){
+            letter = "C";
+        }
+        else if (percentage >= 60){
+            letter = "D";
+        }
+        else if (percentage < 60) {
+            letter = "F";
+        }
+        else {
+            letter = "ERROR";
+        }
+
+        if (percentage >= 70) {
+            passed = true;
+        }
+        else {
+            passed = false;
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (passed) {
+            Console.WriteLine("Congratulations, you have passed!");
+        }
+        else {
+            Console.WriteLine("You did not pass, but don't give up!");
+        }
     }
 }
