45a379dec04ea9842570cb055eb70b44008b7fb1
coolmen444 <isaac.edwards.20022@gmail.com>
2025-01-16 14:56:15 -0700

    Coded guess the number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magic_number = int.Parse(Console.ReadLine());
+    
+        bool continueLoop = true;
+        while (continueLoop) {
+
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            
+            if (guess < magic_number) {
+                Console.WriteLine("Higher");
+            } 
+            else if (guess > magic_number) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess == magic_number) {
+                Console.WriteLine("You guessed it!");
+                continueLoop = false;
+            }
+        }
     }
 }
