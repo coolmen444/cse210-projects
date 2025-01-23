7eb0951f1bf3686c5aab07474d115051e3f42f2d
coolmen444 <isaac.edwards.20022@gmail.com>
2025-01-23 14:10:21 -0700

    Coded assignment- first commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 1998;
+        job1._endYear = 2010;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Janitor";
+        job2._company = "Country Cleaners";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume resume1 = new Resume();
+        resume1._name = "John Sorensen";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        // Console.WriteLine(resume1._jobs[0]._jobTitle);
+
+        resume1.Display();
     }
 }
