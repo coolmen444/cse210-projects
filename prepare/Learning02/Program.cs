7a8bef522e221dd76697365f5ffc270db6d00208
coolmen444 <isaac.edwards.20022@gmail.com>
2025-02-03 20:15:27 -0700

    sandbox changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,26 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
+        Resume theResume = new Resume();
+        bool running = true;
+        while (running) {
+            displayMenu();
+            Console.Write("Enter choice: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1") {
+                Console.Write("What is the name of the company? ");
+                string company = Console.ReadLine();
+
+                Console.Write("What is the position? ");
+                string position = Console.ReadLine();
+
+                Job newJob = new Job();
+                newJob._company = company;
+                newJob._jobTitle = position;
+                newJob._startYear = 2015;
+                newJob._endYear = 2025;
+
+                theResume._jobs.Add(newJob);
+
+            }
+            else if (choice == "2") {
+                theResume.Display();
+            }
+            else if (choice == "3") {
+                //Quit
+                running = false;
+                
+            }
+            else {
+                Console.WriteLine("Sorry, that is not valid. Please try again.");
+            }
+
+        }
+
+        static void displayMenu() {
+            Console.WriteLine("Menu: ");
+            Console.WriteLine(" 1. Add new job");
+            Console.WriteLine(" 2. Display resume");
+            Console.WriteLine(" 3. Quit");
+            Console.WriteLine();
+        }
         
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 1998;
-        job1._endYear = 2010;
+        // Job job1 = new Job();
+        // job1._jobTitle = "Software Engineer";
+        // job1._company = "Microsoft";
+        // job1._startYear = 1998;
+        // job1._endYear = 2010;
 
-        Job job2 = new Job();
-        job2._jobTitle = "Janitor";
-        job2._company = "Country Cleaners";
-        job2._startYear = 2020;
-        job2._endYear = 2022;
+        // Job job2 = new Job();
+        // job2._jobTitle = "Janitor";
+        // job2._company = "Country Cleaners";
+        // job2._startYear = 2020;
+        // job2._endYear = 2022;
 
-        Resume resume1 = new Resume();
-        resume1._name = "John Sorensen";
-        resume1._jobs.Add(job1);
-        resume1._jobs.Add(job2);
+        // Resume resume1 = new Resume();
+        // resume1._name = "John Sorensen";
+        // resume1._jobs.Add(job1);
+        // resume1._jobs.Add(job2);
 
-        // Console.WriteLine(resume1._jobs[0]._jobTitle);
+        // // Console.WriteLine(resume1._jobs[0]._jobTitle);
 
-        resume1.Display();
+        // resume1.Display();
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,115 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._rating = "G";
+        favoriteMovie._runtime = 150;
+
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._rating = "PG-13";
+        otherMovie._runtime = 162;
+
+        // Console.Write(favoriteMovie._title);
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+
+        favoriteMovie.Display();
     }
+
+    static void DisplayMovie(Movie aMovie) 
+    {
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+        return product;
+    }
+    static string GetAdjective() 
+    {
+        List<string> words = new List<string>();
+
+        words.Add("blue");
+        words.Add("yellow");
+        words.Add("big");
+        words.Add("small");
+
+        string adjective = words[2];
+
+        return adjective;
+    }
+
+    static string GetNoun()
+    {
+        string noun = "camel";
+
+        return noun;
+    }
+
 }
+
+
+ //     // This is a comment
+    //     string adjective = GetAdjective();
+    //     string noun = GetNoun();
+    //     int number = Multiply(3,4);
+
+    //     Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s.");
+    //     Console.ForegroundColor = ConsoleColor.Magenta;
+    //     Console.WriteLine(@"
+    //               ,,__
+    //     ..  ..   / o._)                   .---.
+    //    /--'/--\  \-'||        .----.    .'     '.
+    //   /        \_/ / |      .'      '..'         '-.
+    // .'\  \__\  __.'.'     .'          i-._
+    //   )\ |  )\ |      _.'
+    //  // \\ // \\
+    // ||_  \\|_  \\_
+    // '--' '--'' '--'
+    //     ");
+    //     Console.ResetColor();
