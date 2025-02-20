617f6d5450ffac671c9656d403ba38603d2c7442
coolmen444 <isaac.edwards.20022@gmail.com>
2025-02-19 21:10:21 -0700

    Coded inheritance assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Isaac Edwards", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Josh Edwards", "Division", "7.5", "12-15");   
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("John Doe", "Fractions", "How to Divide Like a Pro");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
